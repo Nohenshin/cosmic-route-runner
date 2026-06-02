6ffa85afdcb5a5b878a996e09996e0afdc1b347e
Ho Nhat Thanh <youareyou7.com@gmail.com>
2026-06-03 03:41:22 +0700

    Update game data

--- js/utils/gameData.js
+++ js/utils/gameData.js
@@ -1,7 +1,16 @@
 import { GAME_STATE } from "./gameState.js";
+
 export const gameData = {
     fuel: 100,
     elapsedTime: 0,
     score: 0,
-    state: GAME_STATE.MENU
+    state: GAME_STATE.MENU,
+    boostCooldown: 0,
+    boostActive: false,
+    boostRemaining: 0,
+    gameOverReason: "",
+    winReason: "",
+    health: 10,
+    maxHealth: 10,
+    damagedAsteroids: new Set()
 };
--- js/utils/gameState.js
+++ js/utils/gameState.js
@@ -1,13 +1,7 @@
 export const GAME_STATE = {
-
     MENU: "MENU",
-
     PLAYING: "PLAYING",
-
     PAUSED: "PAUSED",
-
     WIN: "WIN",
-
     GAME_OVER: "GAME_OVER"
-
 };
